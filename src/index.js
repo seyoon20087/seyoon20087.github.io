78afe30a62eac695dd10e8db9b34152753d90f3a
seyoon20087 <24852454+seyoon20087@users.noreply.github.com>
2021-10-27 00:55:55 +0000

    replace with render with createRoot
    Signed-off-by: seyoon20087 <24852454+seyoon20087@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+/*ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+);*/
+
+const root_element = document.getElementById('root');
+
+const root = ReactDOM.createRoot(root_element);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
