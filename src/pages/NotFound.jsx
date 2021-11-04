b20635f90925dfa7774f775d801a9bc764020f07
Shinwon Elizabeth Yoon <24852454+seyoon20087@users.noreply.github.com>
2021-11-04 11:01:43 +0900

    replace `Redirect` with `Navigate`
    Signed-off-by: Shinwon Elizabeth Yoon <24852454+seyoon20087@users.noreply.github.com>

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Redirect } from "react-router-dom";
+import { Navigate } from 'react-router-dom';
 
 export default function NotFound() {
     useEffect(() => {
@@ -7,7 +7,7 @@ export default function NotFound() {
       }, []);
     return (
         <>
-        <Redirect to="/" />
+        <Navigate to="/" />
         </>
     );
 }
