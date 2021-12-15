99cef602a65c22e5609632f84de36091219330ce
Shinwon Elizabeth Yoon <24852454+seyoon20087@users.noreply.github.com>
2021-12-15 18:30:02 +0900

    remove eslint disable line
    Signed-off-by: Shinwon Elizabeth Yoon <24852454+seyoon20087@users.noreply.github.com>

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -16,8 +16,6 @@ const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
-    // 127.0.0.0/8 are considered localhost for IPv4.
-    // eslint-disable-next-line security/detect-unsafe-regex
     window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
 );
 
