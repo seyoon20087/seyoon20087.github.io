aeb30f281a26a9033cf3b6aa6596620c388c89d0
Shinwon Elizabeth Yoon <24852454+seyoon20087@users.noreply.github.com>
2022-01-05 20:32:53 +0900

    update some files
    Signed-off-by: Shinwon Elizabeth Yoon <24852454+seyoon20087@users.noreply.github.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,25 +7,24 @@ function Footer() {
     <>
   <br />
   <footer className="container">
-    Copyright © 2021 github/seyoon20087. All rights reserved.
+    {`Copyright © ${new Date().getFullYear()} github/seyoon20087. All rights reserved.`}
     <br />
-    Use of this site subject to the{" "}
+    {"Use of this site subject to the"}
     <a
       href="https://docs.github.com/github/site-policy/github-terms-of-service"
       target="_blank"
       rel="noopener noreferrer"
     >
-      GitHub Terms of Service
-    </a>{" "}
-    and{" "}
+      {"GitHub Terms of Service"}
+    </a>{" and "}
     <a
       href="https://docs.github.com/github/site-policy/github-privacy-statement"
       target="_blank"
       rel="noopener noreferrer"
     >
-      Privacy Policy
+      {"Privacy Policy"}
     </a>
-    .
+    {"."}
   </footer>
   <br />
   <br />
