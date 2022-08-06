6d7d3ae6a6b0a8035e4c0dcd3b17ff12bcf672b6
Shinwon Elizabeth Yoon <24852454+seyoon20087@users.noreply.github.com>
2022-08-06 17:55:45 +0900

    Remove useless BrowserContextProvider in gatsby-browser.js
    It is included in CommonPage component anyway
    
    Signed-off-by: Shinwon Elizabeth Yoon <24852454+seyoon20087@users.noreply.github.com>

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -5,5 +5,5 @@ import BrowserContextProvider from "./src/components/BrowserContext";
 export const wrapRootElement: GatsbyBrowser["wrapRootElement"] = ({
   element,
 }: WrapRootElementBrowserArgs): React.ReactElement => (
-  <React.StrictMode><BrowserContextProvider>{element}</BrowserContextProvider></React.StrictMode>
+  <React.StrictMode>{element}</React.StrictMode>
 );
