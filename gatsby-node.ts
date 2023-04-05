5dfc4414edfa3f33cfa3faac5a0f29fe478510be
seyoon2008 <24852454+seyoon20087@users.noreply.github.com>
2023-04-05 13:11:48 +0000

    update gatsby-node.ts
    - add babel-loader custom config
    - shim `Object.assign`
    
    Signed-off-by: seyoon2008 <24852454+seyoon20087@users.noreply.github.com>

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,12 +1,70 @@
 import type { GatsbyNode } from "gatsby";
-import type webpack from "webpack";
+import webpack from "webpack";
+import TerserPlugin from "terser-webpack-plugin";
 
 export const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({
   actions,
 }) => {
+  const browserTargets = ">=0%";
   const config: webpack.Configuration = {
     // Disable webpack caching
     cache: undefined,
+    plugins: [
+      new webpack.ProvidePlugin({
+        "Object.assign": "lodash/assign",
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(c|m)?js$/,
+          loader: "babel-loader",
+          enforce: "post",
+          options: {
+            targets: browserTargets,
+            presets: [
+              "@babel/preset-env",
+              ["@babel/preset-react", { runtime: "automatic" }],
+            ],
+          },
+        },
+        {
+          test: /\.m?ts$/,
+          loader: "babel-loader",
+          enforce: "post",
+          options: {
+            targets: browserTargets,
+            presets: ["@babel/preset-env", "@babel/preset-typescript"],
+          },
+        },
+        {
+          test: /\.m?(j|t)sx$/,
+          loader: "babel-loader",
+          enforce: "post",
+          options: {
+            targets: browserTargets,
+            presets: [
+              "@babel/preset-env",
+              ["@babel/preset-react", { runtime: "automatic" }],
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      ],
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            format: {
+              comments: false,
+            },
+          },
+          extractComments: false,
+        }),
+      ],
+    },
   };
   actions.setWebpackConfig(config);
 };
