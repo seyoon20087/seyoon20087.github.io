41f9f49f81ec2ac84540e3f3b070033d3927b1f5
seyoon2008 <24852454+seyoon20087@users.noreply.github.com>
2023-05-02 15:07:20 +0000

    Update gatsby-ssr.tsx
    Use renderToReadableStream for server-rendering
    
    Caveat: the style tags are not removed from the div
    
    Signed-off-by: seyoon2008 <24852454+seyoon20087@users.noreply.github.com>

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,47 +1,63 @@
-import type { GatsbySSR, WrapRootElementNodeArgs } from "gatsby";
-import { renderToString } from "react-dom/server";
-import {
-  EmotionServerCacheContextProvider,
-  EmotionServerCacheContext,
-  serverCache,
-} from "./src/emotion/EmotionCacheContext";
-import React, { useContext } from "react";
+import type { GatsbySSR } from "gatsby";
+import ReactDOMServer from "react-dom/server.browser";
+import React from "react";
 import { CacheProvider } from "@emotion/react";
 import { createEmotionCache } from "./src/emotion/createEmotionCache";
 import createEmotionServer from "@emotion/server/create-instance";
 
-const EmotionCacheWrapper = ({ children }: { children: React.ReactNode }) => {
-  const serverCache = useContext(EmotionServerCacheContext);
-  return (
-    <CacheProvider value={serverCache ?? createEmotionCache()}>
-      {children}
-    </CacheProvider>
-  );
-};
+const { renderToReadableStream } = ReactDOMServer;
+
+function decodeText(input: Uint8Array | undefined, textDecoder: TextDecoder) {
+  return textDecoder.decode(input, { stream: true });
+}
+
+async function streamToString(
+  stream: ReadableStream<Uint8Array>
+): Promise<string> {
+  const reader = stream.getReader();
+  const textDecoder = new TextDecoder();
+
+  let bufferedString = "";
+
+  while (true) {
+    const { done, value } = await reader.read();
+
+    if (done) {
+      return bufferedString;
+    }
+
+    bufferedString += decodeText(value, textDecoder);
+  }
+}
+
+async function renderToStringReadable(element: React.ReactElement) {
+  const renderStream = await renderToReadableStream(element);
+  await renderStream.allReady;
+  return streamToString(renderStream);
+}
 
 export const replaceRenderer: GatsbySSR["replaceRenderer"] = ({
   replaceBodyHTMLString,
   bodyComponent,
   setHeadComponents,
 }) => {
+  const serverCache = createEmotionCache();
   const { extractCriticalToChunks } = createEmotionServer(serverCache);
 
-  const elem = renderToString(
-    <EmotionServerCacheContextProvider>
-      <EmotionCacheWrapper>{bodyComponent}</EmotionCacheWrapper>
-    </EmotionServerCacheContextProvider>
-  );
+  renderToStringReadable(
+    <CacheProvider value={serverCache}>{bodyComponent}</CacheProvider>
+  ).then((resString) => {
+    const emotionStyles = extractCriticalToChunks(resString);
+    const emotionStyleTags = emotionStyles.styles.map((style) => (
+      <style
+        data-emotion={[style.key, style.ids.join(" ")].join(" ")}
+        key={style.key}
+        // nosemgrep:typescript.react.security.audit.react-dangerouslysetinnerhtml.react-dangerouslysetinnerhtml
+        dangerouslySetInnerHTML={{ __html: style.css }}
+      />
+    ));
 
-  const emotionStyles = extractCriticalToChunks(elem);
-  const emotionStyleTags = emotionStyles.styles.map((style) => (
-    <style
-      data-emotion={[style.key, style.ids.join(" ")].join(" ")}
-      key={style.key}
-      // nosemgrep:typescript.react.security.audit.react-dangerouslysetinnerhtml.react-dangerouslysetinnerhtml
-      dangerouslySetInnerHTML={{ __html: style.css }}
-    />
-  ));
-
-  setHeadComponents(emotionStyleTags);
-  replaceBodyHTMLString(elem);
+    setHeadComponents(emotionStyleTags);
+    replaceBodyHTMLString(resString);
+  });
 };
