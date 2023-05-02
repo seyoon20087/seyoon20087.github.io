d92fd1d4c96afb44e2044271ac19d3c7768063d1
seyoon2008 <24852454+seyoon20087@users.noreply.github.com>
2023-05-02 15:40:11 +0000

    update dependencies/webpack config
    Signed-off-by: seyoon2008 <24852454+seyoon20087@users.noreply.github.com>

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -4,8 +4,24 @@ import TerserPlugin from "terser-webpack-plugin";
 
 export const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({
   actions,
+  getConfig,
+  stage,
 }) => {
-  const browserTargets = ">=0%";
+  const targets =
+    stage === ("develop-html" || "build-html")
+      ? "maintained node versions"
+      : ">=0%";
+  const existingConfig: webpack.Configuration = getConfig();
+  ((existingConfig.optimization = existingConfig.optimization || {}).minimizer =
+    (existingConfig.optimization = existingConfig.optimization || {})
+      .minimizer || [])[0] = new TerserPlugin({
+    terserOptions: {
+      format: {
+        comments: false,
+      },
+    },
+    extractComments: false,
+  });
   const config: webpack.Configuration = {
     // Disable webpack caching
     cache: undefined,
@@ -21,7 +37,7 @@ export const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({
           loader: "babel-loader",
           enforce: "post",
           options: {
-            targets: browserTargets,
+            targets,
             presets: [
               "@babel/preset-env",
               ["@babel/preset-react", { runtime: "automatic" }],
@@ -33,7 +49,7 @@ export const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({
           loader: "babel-loader",
           enforce: "post",
           options: {
-            targets: browserTargets,
+            targets,
             presets: ["@babel/preset-env", "@babel/preset-typescript"],
           },
         },
@@ -42,7 +58,7 @@ export const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({
           loader: "babel-loader",
           enforce: "post",
           options: {
-            targets: browserTargets,
+            targets,
             presets: [
               "@babel/preset-env",
               ["@babel/preset-react", { runtime: "automatic" }],
@@ -50,19 +66,6 @@ export const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({
             ],
           },
         },
-      ],
-    },
-    optimization: {
-      minimize: true,
-      minimizer: [
-        new TerserPlugin({
-          terserOptions: {
-            format: {
-              comments: false,
-            },
-          },
-          extractComments: false,
-        }),
       ],
     },
   };
